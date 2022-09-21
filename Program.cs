543326d8a31e06e84f0bd708771f47a46ac375cd
ManishA0799 <manishatyalkar29@gmail.com>
2022-09-21 15:59:01 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] jaggeArray = new int[3][];
+
+            jaggeArray[0] = new int[] { 1, 2, 3, 4, 5 };
+            jaggeArray[1] = new int[] { 1, 2, 3, 4 };
+            jaggeArray[2] = new int[] { 1, 2, 3 };
+
+            for (int i = 0; i < jaggeArray.Length; i++)
+            {
+                for (int j = 0; j < jaggeArray[i].Length; j++)
+                {
+                    Console.Write(jaggeArray[i][j] + "  ");
+
+                }
+                Console.WriteLine();
+            }
+
+           
+
+        }
+    }
+}
